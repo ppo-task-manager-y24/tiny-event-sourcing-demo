c8889d91e2313265937c1a961b0ab75e302e3ca4
Samat Gaynutdinov <samgayn@gmail.com>
2023-12-14 20:22:07 +0300

    fix tests

--- src/test/kotlin/ru/quipy/ProjectsTests.kt
+++ src/test/kotlin/ru/quipy/ProjectsTests.kt
@@ -13,11 +13,13 @@ import org.springframework.http.HttpStatus
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.web.server.ResponseStatusException
 import ru.quipy.domain.Event
+import ru.quipy.logic.ProjectAggregateState
 import ru.quipy.project.ProjectService
 import ru.quipy.project.dto.ProjectCreate
 import ru.quipy.project.dto.ProjectModel
 import ru.quipy.project.eda.api.ProjectAggregate
 import ru.quipy.project.eda.api.ProjectCreatedEvent
+import ru.quipy.user.UserEntity
 import java.sql.Timestamp
 import java.util.*
 
@@ -28,6 +30,11 @@ class ProjectsTests {
         private val participants = MutableList(2, {
             UUID.randomUUID()!!
         })
+
+        private val tasks = MutableList(2, {
+            UUID.randomUUID()!!
+        })
+
         private val name = "projectName"
         private val ownerId = UUID.randomUUID()
         private val createdAt = System.currentTimeMillis()
@@ -39,7 +46,8 @@ class ProjectsTests {
             name,
             ownerId,
             createdAt,
-            updatedAt
+            updatedAt,
+            tasks
         )
 
         private val title = "Project title"
@@ -54,39 +62,63 @@ class ProjectsTests {
     @Autowired
     lateinit var projectEsService: ProjectService
 
+    @Autowired
+    lateinit var mongoTemplate: MongoTemplate
+
+    @BeforeEach
+    fun cleanDatabase() {
+        try {
+            mongoTemplate.remove(Query.query(Criteria.where("projectId").`is`(projectEsService.getOne(id)!!.getId())),
+                ProjectModel::class.java)
+        } catch (e: ResponseStatusException) {
+            if (e.status != HttpStatus.NOT_FOUND)
+            {
+                throw e
+            }
+        }
+    }
+
     @Test
     fun createNewProject() {
-        var event: ProjectCreatedEvent? = null
+        var event: ProjectAggregateState? = null
 
         Assertions.assertDoesNotThrow( {
             event = projectEsService.createOne(projectCreateModel)
         }, "can't create new project")
 
         Assertions.assertAll(
-            Executable { Assertions.assertEquals(event!!.projectId, id, "project ids doesn't match") },
-            Executable { Assertions.assertEquals(event!!.projectName, title,
+            Executable { Assertions.assertEquals(event!!.participants, MutableList<UUID>(0, {
+                UUID.randomUUID()
+            }), "project participants doesn't match") },
+            Executable { Assertions.assertEquals(event!!.name, title,
                 "project names doesn't match") },
-            Executable { Assertions.assertEquals(event!!.projectOwner, ownerId,
+            Executable { Assertions.assertEquals(event!!.owner, ownerId,
             "project owners doesn't match") }
         )
     }
 
     @Test
     fun addUserToProject() {
-        var projectCreatedEvent: ProjectCreatedEvent? = null
+        var projectCreatedEvent: ProjectAggregateState? = null
 
         Assertions.assertDoesNotThrow( {
             projectCreatedEvent = projectEsService.createOne(projectCreateModel)
         }, "can't create new project")
 
-        var userAddEvents: List<Event<ProjectAggregate>>? = null
+        var userAddState: ProjectAggregateState? = null
 
         Assertions.assertDoesNotThrow( {
-            userAddEvents = projectEsService.addUser(projectModel.id, participants[0])
+            userAddState = projectEsService.addUser(projectModel.id, participants[0])
         }, "can't add user")
 
         Assertions.assertAll(
-            Executable { Assertions.assertEquals(2, userAddEvents!!.count(), "wrong number of events") },
+            Executable { Assertions.assertEquals(userAddState!!.participants, MutableList<UUID>(1, {
+                participants[0]
+            }), "project participants doesn't match") },
+            Executable { Assertions.assertEquals(userAddState!!.name, title,
+                "project names doesn't match") },
+            Executable { Assertions.assertEquals(userAddState!!.owner, ownerId,
+                "project owners doesn't match") }
         )
     }
 
--- src/test/kotlin/ru/quipy/TasksTests.kt
+++ src/test/kotlin/ru/quipy/TasksTests.kt
@@ -17,6 +17,7 @@ import ru.quipy.task.eda.TaskService
 import ru.quipy.task.dto.TaskCreate
 import ru.quipy.task.eda.api.TaskAggregate
 import ru.quipy.task.eda.api.TaskCreatedEvent
+import ru.quipy.task.eda.logic.TaskAggregateState
 import java.sql.Timestamp
 import java.util.*
 
@@ -46,60 +47,81 @@ class TasksTests {
 
     @Test
     fun createNewTask() {
-        var event: TaskCreatedEvent? = null
+        var state: TaskAggregateState? = null
 
         Assertions.assertDoesNotThrow( {
-            event = taskEsService.createOne(taskCreateModel)
+            state = taskEsService.createOne(taskCreateModel)
         }, "can't create new task")
 
         Assertions.assertAll(
-            Executable { Assertions.assertEquals(event!!.taskId, taskCreateModel.id, "task ids doesn't match") },
-            Executable { Assertions.assertEquals(event!!.taskName, taskCreateModel.name,
+            Executable { Assertions.assertEquals(state!!.getId(), taskCreateModel.id, "task ids doesn't match") },
+            Executable { Assertions.assertEquals(state!!.name, taskCreateModel.name,
                 "task names doesn't match") },
-            Executable { Assertions.assertEquals(event!!.taskDescription, taskCreateModel.description,
+            Executable { Assertions.assertEquals(state!!.description, taskCreateModel.description,
                 "task descriptions doesn't match") },
-            Executable { Assertions.assertEquals(event!!.projectId, taskCreateModel.projectId,
+            Executable { Assertions.assertEquals(state!!.projectId, taskCreateModel.projectId,
                 "task project ids doesn't match") },
-            Executable { Assertions.assertEquals(event!!.statusId, taskCreateModel.statusId,
+            Executable { Assertions.assertEquals(state!!.statusId, taskCreateModel.statusId,
                 "task status ids doesn't match") }
         )
     }
 
     @Test
     fun renameTask() {
-        var taskCreatedEvent: TaskCreatedEvent? = null
+        var taskCreatedState: TaskAggregateState? = null
 
         Assertions.assertDoesNotThrow( {
-            taskCreatedEvent = taskEsService.createOne(taskCreateModel)
+            taskCreatedState = taskEsService.createOne(taskCreateModel)
         }, "can't create new task")
 
-        var taskRenameEvents: List<Event<TaskAggregate>>? = null
+        var taskRenameState: TaskAggregateState? = null
 
         Assertions.assertDoesNotThrow( {
-            taskRenameEvents = taskEsService.rename(taskCreateModel.id, newTaskName)
+            taskRenameState = taskEsService.rename(taskCreateModel.id, newTaskName)
         }, "can't rename task")
 
         Assertions.assertAll(
-            Executable { Assertions.assertEquals(taskRenameEvents!!.count(), 1, "wrong number of events") },
+            Executable { Assertions.assertEquals(taskRenameState!!.getId(), taskCreateModel.id, "task ids doesn't match") },
+            Executable { Assertions.assertEquals(taskRenameState!!.name, newTaskName,
+                "task names doesn't match") },
+            Executable { Assertions.assertEquals(taskRenameState!!.description, taskCreateModel.description,
+                "task descriptions doesn't match") },
+            Executable { Assertions.assertEquals(taskRenameState!!.projectId, taskCreateModel.projectId,
+                "task project ids doesn't match") },
+            Executable { Assertions.assertEquals(taskRenameState!!.statusId, taskCreateModel.statusId,
+                "task status ids doesn't match") }
         )
     }
 
     @Test
     fun addUser() {
-        var taskCreatedEvent: TaskCreatedEvent? = null
+
+        var taskCreatedState: TaskAggregateState? = null
 
         Assertions.assertDoesNotThrow( {
-            taskCreatedEvent = taskEsService.createOne(taskCreateModel)
+            taskCreatedState = taskEsService.createOne(taskCreateModel)
         }, "can't create new task")
 
-        var addUserEvents: List<Event<TaskAggregate>>? = null
+        var addUserState: TaskAggregateState? = null
 
         Assertions.assertDoesNotThrow( {
-            addUserEvents = taskEsService.addUser(taskCreateModel.id, assigneeId)
+            addUserState = taskEsService.addUser(taskCreateModel.id, assigneeId)
         }, "can't rename task")
 
         Assertions.assertAll(
-            Executable { Assertions.assertEquals(2, addUserEvents!!.count(), "wrong number of events") },
+            Executable { Assertions.assertEquals(addUserState!!.getId(), taskCreateModel.id, "task ids doesn't match") },
+            Executable { Assertions.assertEquals(addUserState!!.name, newTaskName,
+                "task names doesn't match") },
+            Executable { Assertions.assertEquals(addUserState!!.description, taskCreateModel.description,
+                "task descriptions doesn't match") },
+            Executable { Assertions.assertEquals(addUserState!!.projectId, taskCreateModel.projectId,
+                "task project ids doesn't match") },
+            Executable { Assertions.assertEquals(addUserState!!.executors.count(), 1,
+                "task executors doesn't match") },
+            Executable { Assertions.assertEquals(addUserState!!.executors[0], assigneeId,
+                "task executors id doesn't match") },
+            Executable { Assertions.assertEquals(addUserState!!.statusId, taskCreateModel.statusId,
+                "task status ids doesn't match") }
         )
     }
 
