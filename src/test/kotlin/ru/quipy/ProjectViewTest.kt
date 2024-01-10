9a1c0d740a4e52bef6ef20c55f47ee0aac487133
Ruslan Khakimov <ruslankhakimov67@gmail.com>
2024-01-10 14:32:16 +0300

    add projection logic test

--- src/test/kotlin/ru/quipy/ProjectViewTest.kt
+++ src/test/kotlin/ru/quipy/ProjectViewTest.kt
@@ -26,10 +26,7 @@ import ru.quipy.projections.project_view.ProjectViewService
 import ru.quipy.projections.task_view.TaskViewEntity
 import ru.quipy.projections.user_project_view.UserProjectViewEntity
 import ru.quipy.status.eda.api.StatusAggregate
-import ru.quipy.status.eda.logic.StatusAggregateState
-import ru.quipy.status.eda.logic.addStatus
-import ru.quipy.status.eda.logic.create
-import ru.quipy.status.eda.logic.delete
+import ru.quipy.status.eda.logic.*
 import ru.quipy.status.eda.projections.status_view.StatusService
 import ru.quipy.status.eda.projections.status_view.StatusViewEntity
 import ru.quipy.user.UserEntity
@@ -366,6 +363,119 @@ class ProjectViewTest {
                 Assertions.assertEquals(taskNames, project.tasks.toMutableList())
                 Assertions.assertEquals(statusNames, project.statuses.toMutableList())
             }
-        
+    }
+
+    @Test
+    fun UserAggrProjectAggrSync() {
+        val userId1 = UUID.randomUUID()
+        val username1 = "UserAggrProjectAggrSync1"
+        userEsService.create {
+            it.create(
+                userId1,
+                username1,
+                "",
+                ""
+            )
+        }
+
+        Awaitility
+            .await()
+            .pollDelay(1, TimeUnit.SECONDS)
+
+        projectService.addUserToProject(projectId, userId1)
+
+        Awaitility
+            .await()
+            .pollDelay(1, TimeUnit.SECONDS)
+            .untilAsserted {
+                val projects = projectViewService.getParticipants(projectId)
+
+                Assertions.assertTrue(projects.contains(username1))
+            }
+
+        val userId2 = UUID.randomUUID()
+        val username2 = "UserAggrProjectAggrSync2"
+        projectService.addUserToProject(projectId, userId2)
+
+        Awaitility
+            .await()
+            .pollDelay(1, TimeUnit.SECONDS)
+
+        userEsService.create {
+            it.create(
+                userId2,
+                username2,
+                "",
+                ""
+            )
+        }
+
+        Awaitility
+            .await()
+            .pollDelay(1, TimeUnit.SECONDS)
+            .untilAsserted {
+                val projects = projectViewService.getParticipants(projectId)
+
+                Assertions.assertTrue(projects.contains(username2))
+            }
+    }
+
+    @Test
+    fun StatusAggrProjectAggrSync() {
+        val statusId1 = UUID.randomUUID()
+        val statusName1 = "StatusAggrProjectAggrSync1"
+        statusEsService.update(projectId) {
+            it.addStatus(statusId1, statusName1, 0)
+        }
+        val taskId1 = UUID.randomUUID()
+        val taskName1 = "StatusAggrProjectAggrSync1_task"
+        projectService.createTask(TaskCreate(taskId1, taskName1, "", projectId, statuses[0]))
+
+        Awaitility
+            .await()
+            .pollDelay(1, TimeUnit.SECONDS)
+
+        statusEsService.update(projectId) {
+            it.changeStatusForTask(taskId1, statusId1)
+        }
+
+        Awaitility
+            .await()
+            .pollDelay(1, TimeUnit.SECONDS)
+            .untilAsserted {
+                val statuses_ = projectViewService.getStatuses(projectId)
+                Assertions.assertTrue(statuses_.contains(statusName1))
+
+                val tasks_ = projectViewService.getTasks(projectId)
+                tasks_.any { task -> task.name == taskName1 && task.statusId == statusId1 }
+            }
+
+        val statusId2 = UUID.randomUUID()
+        val statusName2 = "StatusAggrProjectAggrSync2"
+        statusEsService.update(projectId) {
+            it.addStatus(statusId2, statusName2, 0)
+        }
+        val taskId2 = UUID.randomUUID()
+        val taskName2 = "StatusAggrProjectAggrSync2_task"
+        statusEsService.update(projectId) {
+            it.changeStatusForTask(taskId2, statusId2)
+        }
+
+        Awaitility
+            .await()
+            .pollDelay(1, TimeUnit.SECONDS)
+
+        projectService.createTask(TaskCreate(taskId2, taskName2, "", projectId, statuses[0]))
+
+        Awaitility
+            .await()
+            .pollDelay(1, TimeUnit.SECONDS)
+            .untilAsserted {
+                val statuses_ = projectViewService.getStatuses(projectId)
+                Assertions.assertTrue(statuses_.contains(statusName2))
+
+                val tasks_ = projectViewService.getTasks(projectId)
+                tasks_.any { task -> task.name == taskName2 && task.statusId == statusId2 }
+            }
     }
 }
